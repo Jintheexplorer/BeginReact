b00d357da3963edaa61e915915d6d48f21b39b35
Jintheexplorer <45712175+Jintheexplorer@users.noreply.github.com>
2020-12-22 12:28:30 +0900

    Add CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <h1>안녕하세요 리액트</h1>
       </div>
     );
