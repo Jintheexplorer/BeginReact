4e9f624c25e314bb59a10a3622b44d6d2ce1189e
Jintheexplorer <45712175+Jintheexplorer@users.noreply.github.com>
2020-12-22 14:11:05 +0900

    LifeCycle API

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 
+const Problematic = () => {
+  throw new Error('버그가 나타났다!');
+  return <div></div>;
+};
+
 class Counter extends Component {
   state = {
     number: 0
   };
 
+  constructor(props) {
+    super(props);
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // 5의 배수라면 리렌더링 하지 않음
+    console.log('shouldComponentUpdate');
+    if (nextState.number % 5 === 0) return false;
+    return true;
+  }
+
+  componentWillUpadate(nextProps, nextState) {
+    console.log('componentWillUpdate');
+  }
+
+  componentDidUpdate(prevProps, PrevState) {
+    console.log('compoenentDidUpdate');
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({
+      error: true
+    });
+  }
+
   handleIncrease = () => {
+    const { number } = this.state;
     this.setState({
       number: this.state.number + 1
     });
@@ -18,6 +54,7 @@ class Counter extends Component {
   };
 
   render() {
+    console.log('render');
     return (
       <div>
         <h1>카운터</h1>
