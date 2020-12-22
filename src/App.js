5d2cf6090836869a8d89c5bea8e06bc0645f6cd5
Jintheexplorer <45712175+Jintheexplorer@users.noreply.github.com>
2020-12-22 12:36:18 +0900

    Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import MyName from './MyName';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>안녕하세요 리액트</h1>
+        {/* 주석은 이렇게 */}
+        <MyName name="리액트" />
       </div>
     );
   }
